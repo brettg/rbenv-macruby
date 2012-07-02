efde98b22db01e9a510b54c7b3baaa1b0450400b
Brett Gibson <bdg@brettdgibson.com>
2012-07-02 18:08:06 -0500

    don't override gem home set by rbenv-gemset

--- etc/rbenv.d/exec/macruby.bash
+++ etc/rbenv.d/exec/macruby.bash
@@ -1,9 +1,11 @@
 
 if [ "$(rbenv-version-name | sed 's/-.*$//')" = "macruby" ];then
-  # TODO - check if GEM_HOME has been set by rbenv-gemset
-  unset GEM_HOME GEM_PATH
-  path=$(rbenv-prefix)/lib
-  GEM_HOME="$path"
-  GEM_PATH="$path"
-  export GEM_HOME GEM_PATH
+  # If rbenv-gemset has set the gem home, let it be
+  if [ ${GEM_HOME/gemset/} = ${GEM_HOME} ];then
+    unset GEM_HOME GEM_PATH
+    path=$(rbenv-prefix)/lib
+    GEM_HOME="$path"
+    GEM_PATH="$path"
+    export GEM_HOME GEM_PATH
+  fi
 fi
